bc8c700f2a8a94f5d84769f867dd155f0691d952
Mahendra Shahu <mahendrashahu919@gmail.com>
2023-10-05 16:08:17 +0530

    first commit

---
+++ src/components/Forms/Login.js
@@ -0,0 +1,56 @@
+import React, {Component} from 'react';
+import fire from '../../config/Fire';
+import './Login.css';
+
+
+class Login extends Component {
+    state = {
+        email: '',
+        password: '',
+        fireErrors: ''
+    }
+
+    login = e => {
+        e.preventDefault();
+        fire.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
+        .catch((error) => {
+            this.setState({fireErrors: error.message})
+        });
+    }
+
+    handleChange = e => {
+        this.setState({[e.target.name]: e.target.value});
+    }
+
+    render() {
+
+        let errorNotification = this.state.fireErrors ? 
+            ( <div className="Error"> The password is invalid or the user does not have a password. </div> ) : null;
+
+        
+        return (
+            <>
+                {errorNotification}
+                <form>
+                    <input type="text"
+                        className="regField"
+                        placeholder="Email"
+                        value={this.state.email} 
+                        onChange={this.handleChange}
+                        name="email"
+                        />
+                    <input
+                        className="regField"
+                        placeholder="Pasword"
+                        value={this.state.password} 
+                        onChange={this.handleChange}
+                        name="password"
+                        type="password"
+                    />
+                    <input className="submitBtn" type="submit" onClick={this.login} value="ENTER" />
+                </form>
+            </>
+        );
+    }
+}
+export default Login;
